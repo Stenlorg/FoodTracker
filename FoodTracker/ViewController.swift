ecc9fd705fedfffc6227801c37aa451d11004ef0
Stepan Grigoryev <s-grigoryev@yandex-team.ru>
2020-08-04 21:14:04 +0300

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Stepan Grigoryev on 04.08.2020.
+//  Copyright © 2020 Stepan Grigoryev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
